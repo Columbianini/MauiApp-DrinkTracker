4135c1f5840731155ee8d979459344ab42dde812
Tony An <13816554795@163.com>
2022-07-16 20:46:16 +0800

    refactor: clean unnecessary codes

--- DrinkDrink/View/MainPage.xaml.cs
+++ DrinkDrink/View/MainPage.xaml.cs
@@ -10,19 +10,5 @@ public partial class MainPage : ContentPage
 		viewModel.GetAllHistoryCommand.Execute(null);
 	}
 
-	//private void Button_Clicked(object sender, EventArgs e)
-	//{
-	//	var viewmodel= (TodayDrinkViewModel)BindingContext;
-	//	if(viewmodel.cupOnHand == 0)
-	//	{
-	//		viewmodel.StartDrinkCommand.Execute(null);
-	//		this.ToggleButton.Source = "fullbottle.png";
- //       }
-	//	else
-	//	{
-	//		viewmodel.FinishDrinkCommand.Execute(null);
-	//		this.ToggleButton.Source = "emptybottle.png";
-	//	}
-	//}
 }
 
