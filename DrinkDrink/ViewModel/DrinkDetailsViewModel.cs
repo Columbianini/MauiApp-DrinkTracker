190be0cbac24a6a6219f795ef18db37a4720c0ae
Tony An <13816554795@163.com>
2022-07-26 00:08:04 +0800

    TODO: Add  ToDo List at DirnkDetailsViewModel.cs

--- DrinkDrink/ViewModel/DrinkDetailsViewModel.cs
+++ DrinkDrink/ViewModel/DrinkDetailsViewModel.cs
@@ -14,3 +14,10 @@ namespace DrinkDrink.ViewModel
         List<Cup> cups;
     }
 }
+
+// TODO:
+// 1. Add TotalDrinkSeconds List at TodayDrinkViewModel at Line 158
+// 2. Add Another Query Property [QueryProperty("TotalDrinkSeconds", "TotalDrinkSeconds")]
+// 3. Add totalDrinkSeconds as a ObservableProperty at line 15 in the file
+// 4. Update DrinkPage to show totaldrinkseconds
+// 5. Check  to add step line plot, if no data gives none https://lvcharts.com/docs/maui/2.0.0-beta.300/CartesianChart.Cartesian%20chart%20control
